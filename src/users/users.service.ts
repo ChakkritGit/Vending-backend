671cb5c1c9172315fdb944a59c2b2d3dc22c4ebd
ChakkritGit <nongtonnee@gmail.com>
2024-12-12 11:16:40 +0700

    update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,44 +5,69 @@ import { Users } from '@prisma/client';
 import { v4 as uuidv4 } from 'uuid';
 import { getDateFormat } from 'src/utils/date.format';
 import * as bcrypt from 'bcrypt';
+import * as fs from 'fs/promises';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) { }
 
+  private async deleteFile(filePath: string) {
+    try {
+      await fs.unlink(`.${filePath}`);
+      console.log(`🗑️  this image has been deleted [${filePath}]`)
+    } catch (error) {
+      console.error(`Failed to delete file: ${filePath}`, error.message);
+    }
+  }
+
   async create(createUserDto: Users) {
-    const { username, password, display, picture, role, comment } = createUserDto
+    const { username, password, display, picture, role, comment } = createUserDto;
+
     const userExits = await this.prisma.users.findUnique({
-      where: {
-        username: username
-      }
-    })
-    if (userExits) throw new HttpException('This username is already in use!', HttpStatus.BAD_REQUEST);
-    const hashedPassword = await bcrypt.hash(password, 10); // saltRounds = 10
-    const result = await this.prisma.users.create({
-      select: {
-        id: true,
-        username: true,
-        display: true,
-        picture: true,
-        role: true,
-        status: true,
-        createdAt: true,
-        updatedAt: true
-      },
-      data: {
-        id: `UID-${uuidv4()}`,
-        username: username,
-        password: hashedPassword,
-        display: display,
-        picture: picture,
-        role: role,
-        comment: comment,
-        createdAt: getDateFormat(new Date()),
-        updatedAt: getDateFormat(new Date())
-      }
-    })
-    return result;
+      where: { username: username },
+    });
+
+    if (userExits) {
+      if (picture) await this.deleteFile(picture);
+      throw new HttpException('This username is already in use!', HttpStatus.BAD_REQUEST);
+    }
+
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const result = await this.prisma.users.create({
+        select: {
+          id: true,
+          username: true,
+          display: true,
+          picture: true,
+          role: true,
+          status: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+        data: {
+          id: `UID-${uuidv4()}`,
+          username: username,
+          password: hashedPassword,
+          display: display,
+          picture: picture,
+          role: role,
+          comment: comment,
+          createdAt: getDateFormat(new Date()),
+          updatedAt: getDateFormat(new Date()),
+        },
+      });
+
+      return result;
+    } catch (error) {
+      if (picture) await this.deleteFile(picture);
+
+      throw new HttpException(
+        'An error occurred while creating the user!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   findAll() {

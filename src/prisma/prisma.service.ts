0839535495d4befd6882d9b3d36d0ef7621f4852
ChakkritGit <nongtonnee@gmail.com>
2025-01-13 12:49:23 +0700

    update

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -18,13 +18,13 @@ export class PrismaService
   async onModuleInit (): Promise<void> {
     try {
       await this.$connect()
-      logger.log('PrismaService connected successfully.')
+      logger.log('Database connected successfully.')
       this.isRetrying = false
     } catch (error) {
-      logger.error('PrismaService connection error:', error)
+      logger.error('Database connection error:', error)
       if (!this.isRetrying) {
         this.isRetrying = true
-        logger.warn('PrismaService re-connection in 5 seconds...')
+        logger.warn('Database re-connection in 5 seconds...')
         setTimeout(() => this.onModuleInit(), 5000)
       }
     }
@@ -32,7 +32,7 @@ export class PrismaService
 
   async onApplicationShutdown (signal?: string): Promise<void> {
     logger.warn(
-      `Application is shutting down (${signal}). Disconnecting PrismaService.`,
+      `Application is shutting down (${signal}). Disconnecting Database.`,
     )
     await this.$disconnect()
   }
